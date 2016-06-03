c32cf4d674db46189f7c5948f4ddb8ea0ff0a4ca
wuwei <wwhihi2008@163.com>
2016-06-03 10:51:14 +0800

    添加几种常见的上传请求的请求体MIME扩展

--- FrameworkV1/FrameworkV1/Service/Business/RemoteRequest/HTTP/Request/HTTPRequestBody+MIME.m
+++ FrameworkV1/FrameworkV1/Service/Business/RemoteRequest/HTTP/Request/HTTPRequestBody+MIME.m
@@ -1,15 +1,15 @@
 //
-//  HTTPRequestBody+BodyType.m
+//  HTTPRequestBody+MIME.m
 //  FrameworkV1
 //
 //  Created by ww on 16/5/19.
 //  Copyright © 2016年 WW. All rights reserved.
 //
 
-#import "HTTPRequestBody+BodyType.h"
+#import "HTTPRequestBody+MIME.h"
 #import "HTTPConnectionInputStream+HTTPMultipartForm.h"
 
-@implementation HTTPRequestBody (BodyType)
+@implementation HTTPRequestBody (MIME)
 
 + (HTTPRequestBody *)requestBodyWithParameters:(NSDictionary<NSString *,NSString *> *)parameters
 {
@@ -39,13 +39,24 @@
     return body;
 }
 
++ (HTTPRequestBody *)requestBodyWithText:(NSString *)text
+{
+    HTTPRequestDataBody *body = [[HTTPRequestDataBody alloc] init];
+    
+    body.contentType = @"text/plain;charset=utf-8";
+    
+    body.data = [text dataUsingEncoding:NSUTF8StringEncoding];
+    
+    return body;
+}
+
 + (HTTPRequestBody *)requestBodyWithJsonNode:(id)jsonNode
 {
     NSData *data = jsonNode ? [NSJSONSerialization dataWithJSONObject:jsonNode options:0 error:nil] : nil;
     
     HTTPRequestDataBody *body = [[HTTPRequestDataBody alloc] init];
     
-    body.contentType = @"application/json";
+    body.contentType = @"application/json;charset=utf-8";
     
     body.data = data;
     
@@ -63,6 +74,50 @@
     body.contentType = [NSString stringWithFormat:@"multipart/form-data; boundary=%@", entity.boundary];
     
     body.stream = stream;
+    
+    return body;
+}
+
++ (HTTPRequestBody *)requestBodyWithPNGImageData:(NSData *)PNGImageData
+{
+    HTTPRequestDataBody *body = [[HTTPRequestDataBody alloc] init];
+    
+    body.contentType = @"image/png";
+    
+    body.data = PNGImageData;
+    
+    return body;
+}
+
++ (HTTPRequestBody *)requestBodyWithPNGImageFile:(NSURL *)PNGImageFile
+{
+    HTTPRequestFileBody *body = [[HTTPRequestFileBody alloc] init];
+    
+    body.contentType = @"image/png";
+    
+    body.fileURL = PNGImageFile;
+    
+    return body;
+}
+
++ (HTTPRequestBody *)requestBodyWithJPGImageData:(NSData *)JPGImageData
+{
+    HTTPRequestDataBody *body = [[HTTPRequestDataBody alloc] init];
+    
+    body.contentType = @"image/jpeg";
+    
+    body.data = JPGImageData;
+    
+    return body;
+}
+
++ (HTTPRequestBody *)requestBodyWithJPGImageFile:(NSURL *)JPGImageFile
+{
+    HTTPRequestFileBody *body = [[HTTPRequestFileBody alloc] init];
+    
+    body.contentType = @"image/jpeg";
+    
+    body.fileURL = JPGImageFile;
     
     return body;
 }
